7664f162796769777401d18c06104900e025d041
Rodrigo Giardino <giardino.rodrigo@outlook.com>
2017-09-11 14:15:06 -0300

    Prototype's changes.

--- prototypes/windows-resizing/scr_size_test.py
+++ prototypes/windows-resizing/scr_size_test.py
@@ -4,26 +4,22 @@ scr_size = 1010, 691
 screen = pygame.display.set_mode(scr_size)
 pygame.display.set_caption('Screen Size Test')
 
-picture = pygame.image.load('entrada_do_almoxarifado.png')
+picture = pygame.image.load('departamentos.png')
 
-porta_1 = pygame.image.load('porta_do_almoxarifado1.png')
-porta_2 = pygame.image.load('porta_do_almoxarifado2.png')
-porta_3 = pygame.image.load('porta_do_almoxarifado3.png')
+porta_1 = pygame.image.load('porta_do_DM1.png')
+porta_2 = pygame.image.load('porta_do_DM2.png')
 
 no = 0
-portas = [porta_1, porta_2, porta_3]
+portas = [porta_1, porta_2]
 porta_atual = portas[no]
 
 porta_rect_1 = porta_1.get_rect()
-porta_rect_1.topleft = (408, 12)
+porta_rect_1.topleft = (689, 95)
 
 porta_rect_2 = porta_2.get_rect()
-porta_rect_2.topleft = (408, 12)
+porta_rect_2.topleft = (690, 95)
 
-porta_rect_3 = porta_3.get_rect()
-porta_rect_3.topleft = (408, 7)
-
-portas_r = [porta_rect_1, porta_rect_2, porta_rect_3]
+portas_r = [porta_rect_1, porta_rect_2]
 porta_atual_r = portas_r[no]
 
 inventory = pygame.image.load('inventario.png')
@@ -41,7 +37,7 @@ while not done:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 no += 1
-                if no > 2:
+                if no > 1:
                     no = 0
 
                 porta_atual = portas[no]
