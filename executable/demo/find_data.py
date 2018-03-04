e34e05405f0b1cd8f4625a2b12f4c6907a101399
Rodrigo Giardino <giardino.rodrigo@outlook.com>
2018-03-04 12:22:25 -0300

    Executable changes.

--- executable/demo/find_data.py
+++ executable/demo/find_data.py
@@ -4,7 +4,7 @@ import sys
 def find_data_file(filename):
     if getattr(sys, 'frozen', False):
         # The application is frozen
-        datadir = os.path.dirname(sys.executable)
+        datadir = sys._MEIPASS
     else:
         # The application is not frozen
         # Change this bit to match where you store your data files:
